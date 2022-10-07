6b622d3746ad75b9ecfcec7658c16ef6c7949143
byval <byvaltsev1978@gmail.com>
2022-10-07 14:40:39 +0300

    Some properties for Spring Actuator

--- src/main/java/com/byval/labs/railwaytransport/RailwayTransportApplication.java
+++ src/main/java/com/byval/labs/railwaytransport/RailwayTransportApplication.java
@@ -1,13 +1,17 @@
 package com.byval.labs.railwaytransport;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.info.MapInfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @SpringBootApplication
 public class RailwayTransportApplication {
@@ -31,4 +35,17 @@ public class RailwayTransportApplication {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+
+    @Bean
+    InfoContributor getInfoContributor() {
+        Map<String, Object> details = new HashMap<>();
+        details.put("nameApp", "Railway Transport");
+        details.put("description", "It`s a Java Spring Boot application");
+        details.put("developer", "Nikolai Byvaltsev");
+        details.put("email", "nikolai-byvaltsev@email.com");
+        Map<String, Object> wrapper = new HashMap<>();
+        wrapper.put("info", details);
+        return new MapInfoContributor(wrapper);
+    }
+
 }
